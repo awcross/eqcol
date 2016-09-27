1c8ceae13e439870e6a5f6a65ee4e91ef6f9a208
Alex Cross <crossaw@gmail.com>
2016-09-26 23:21:02 -0700

    Add jQuery plugin

---
+++ src/jquery-eqcol.js
@@ -0,0 +1,19 @@
+/* eslint-env jquery */
+import E from './eqcol';
+
+$.fn.eqcol = function (opts) {
+	return this.each((index, element) => {
+		const $this = $(element);
+		const options = $.extend({}, typeof opts === 'object' && opts);
+
+		let instance = $this.data('eqcolizer');
+
+		if (!instance) {
+			$this.data('eqcolizer', (instance = new E(element, options)));
+		}
+
+		if (typeof opts === 'string') {
+			instance[opts]();
+		}
+	});
+};
